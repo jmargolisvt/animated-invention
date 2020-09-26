63f68b4996c4e71e050419f57e1d08bdd99eb054
jmargolisvt <jeff@jmargolisvt.com>
2020-09-25 21:44:58 -0400

    set up

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import middleware, { sagaMiddleware } from './middleware';
 
-export default configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const configStore = (initialState = {}) => {
+  const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(...middleware)));
+
+  sagaMiddleware.run(rootSaga);
+
+  if (module.hot) {
+    module.hot.accept('reducers', () => {
+      store.replaceReducer(require('reducers/index').default);
+    });
+  }
+
+  return {
+    store,
+  };
+};
+
+const { store } = configStore();
